ac114091efbb9f260f2655de07121030d68f421f
kaguilar94m <kaguilar94m@gmail.com>
2016-04-24 14:34:31 -0600

    Modificacion clase GrupoMateriaInsertarActivity

--- app/src/main/java/proyecto/pdm/GrupoMateriaInsertarActivity.java
+++ app/src/main/java/proyecto/pdm/GrupoMateriaInsertarActivity.java
@@ -1,13 +1,38 @@
 package proyecto.pdm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
 
-public class GrupoMateriaInsertarActivity extends AppCompatActivity {
+public class GrupoMateriaInsertarActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    EditText editTipoGrupo;
+    EditText editIdGrupo;
+    EditText editMateria;
+    EditText editDocente;
+    EditText editCiclo;
+    EditText editLocal;
+    EditText editDiasImpartida;
+    EditText editHorario;
+    EditText editNumGrupo;
+
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grupo_materia_insertar);
+
+        editTipoGrupo = (EditText) findViewById(R.id.editTipoGrupo);
+        editIdGrupo = (EditText) findViewById(R.id.editIdGrupo);
+        editMateria = (EditText) findViewById(R.id.editMateria);
+        editDocente = (EditText) findViewById(R.id.editDocente);
+        editCiclo = (EditText) findViewById(R.id.editCiclo);
+        editLocal = (EditText) findViewById(R.id.editIdGrupo);
+        editDiasImpartida = (EditText) findViewById(R.id.editMateria);
+        editHorario= (EditText) findViewById(R.id.editDocente);
+        editNumGrupo= (EditText) findViewById(R.id.editCiclo);
+
+
     }
 }
