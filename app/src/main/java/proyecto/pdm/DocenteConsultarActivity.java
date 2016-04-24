cfd9c2cc1e8a249aa17c7a19472c30b55cf57e3b
kaguilar94m <kaguilar94m@gmail.com>
2016-04-23 21:20:32 -0600

    Cambio en algunos atributos de las tablas(GrupoMateria y Reserva), los formularios de consultar, actualizar y eliminar  de las tablas(GrupoMateria, Docente, Materia)

--- app/src/main/java/proyecto/pdm/DocenteConsultarActivity.java
+++ app/src/main/java/proyecto/pdm/DocenteConsultarActivity.java
@@ -3,7 +3,7 @@ package proyecto.pdm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DocenteConsultarActualizar extends AppCompatActivity {
+public class DocenteConsultarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
