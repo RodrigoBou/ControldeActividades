820fa4886c3f790c1a4bc7b0ef0efcf2051438d9
kaguilar94m <kaguilar94m@gmail.com>
2016-05-05 14:46:53 -0600

    Archivos modificados: -DocenteConsultarActivity -DocenteInsertarActivity -GrupoMateriaConsultarActivity -GrupoMateriaInsertarActivity -MateriaConsultarActivity -MateriaInsertarActivity.

--- app/src/main/java/proyecto/pdm/MateriaInsertarActivity.java
+++ app/src/main/java/proyecto/pdm/MateriaInsertarActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import proyecto.pdm.ClasesModelo.Materia;
 
 public class MateriaInsertarActivity extends Activity {
-
+    ControlBD helper;
     EditText editNomMateria;
     EditText editCodMateria;
 
@@ -14,8 +17,22 @@ public class MateriaInsertarActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materia_insertar);
+        helper = new ControlBD(this);
         editCodMateria = (EditText) findViewById(R.id.editCodMateria);
         editNomMateria = (EditText) findViewById(R.id.editNomMateria);
+    }
+
+    public void insertarMateria(View v){
+        String codMateria = editCodMateria.getText().toString();
+        String nomMateria = editNomMateria.getText().toString();
+        String regInsertados;
+        Materia materia = new Materia();
+        materia.setCodMateria(codMateria);
+        materia.setNomMateria(nomMateria);
+        helper.abrir();
+      //  regInsertados = helper.insertar(materia);
+        helper.cerrar();
+       // Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
     }
 
     public void limpiarTexto(View v){
