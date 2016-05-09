7a47f0ae5f1b8ccb78c048b34f76804210c5573d
kaguilar94m <kaguilar94m@gmail.com>
2016-05-09 11:50:06 -0600

    Archivos modificados: -DocenteMenuActivity -GrupoMateriaMenuActivity -MateriaMenuActivity.

--- app/src/main/java/proyecto/pdm/MateriaMenuActivity.java
+++ app/src/main/java/proyecto/pdm/MateriaMenuActivity.java
@@ -1,13 +1,41 @@
 package proyecto.pdm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MateriaMenuActivity extends AppCompatActivity {
+public class MateriaMenuActivity extends ListActivity {
+    String[] menu={"Insertar Registro","Eliminar Registro","Consultar Registro","Actualizar Registro"};
+    String[] activities = {"MateriaInsertarActivity", "MateriaEliminarActivity","MateriaConsultarActivity", "MateriaActualizarActivity"};
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_materia_menu);
+    public void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.rgb(246, 137, 42));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        String nombreValue = activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.rgb(249, 169, 99));
+
+        try {
+            Class<?> clase = Class.forName("proyecto.pdm." + nombreValue);
+            Intent inte = new Intent(this, clase);
+            this.startActivity(inte);
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
