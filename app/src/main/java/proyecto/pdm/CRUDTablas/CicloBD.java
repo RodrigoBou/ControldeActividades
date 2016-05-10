16fba910e284c81f0da4064f32cccb2f19140396
geovannyPaiz <geovanny_paiz@hotmail.com>
2016-05-10 16:09:27 -0600

    CragaAcademicaInsertar

---
+++ app/src/main/java/proyecto/pdm/CRUDTablas/CicloBD.java
@@ -0,0 +1,12 @@
+package proyecto.pdm.CRUDTablas;
+
+import proyecto.pdm.CargaAcademicaInsertarActivity;
+
+/**
+ * Created by pc on 10/05/2016.
+ */
+public class CicloBD {
+    public CicloBD(CargaAcademicaInsertarActivity cargaAcademicaInsertarActivity) {
+
+    }
+}
--- app/src/main/java/proyecto/pdm/CargaAcademicaInsertarActivity.java
+++ app/src/main/java/proyecto/pdm/CargaAcademicaInsertarActivity.java
@@ -4,51 +4,80 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.List;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
 import proyecto.pdm.CRUDTablas.CargaAcademicaBD;
+import proyecto.pdm.CRUDTablas.CargoDB;
+import proyecto.pdm.CRUDTablas.CicloBD;
+import proyecto.pdm.CRUDTablas.DocenteBD;
+import proyecto.pdm.CRUDTablas.MateriaBD;
 import proyecto.pdm.ClasesModelo.CargaAcademica;
+import proyecto.pdm.ClasesModelo.Docente;
+import proyecto.pdm.ClasesModelo.Materia;
 
 public class CargaAcademicaInsertarActivity extends Activity {
-    CargaAcademicaBD helper;
-    EditText editCargo;
-    EditText editDocente;
-    EditText editMateria;
-    EditText editCiclo;
-
+    private Spinner SpinDocente;
+    private Spinner SpinCargo;
+    private Spinner SpinCiclo;
+    private Spinner SpinMateria;
+    private CargaAcademicaBD helper;
+    private DocenteBD docenteBD;
+    private CicloBD cicloBD;
+    private CargoDB cargoBD;
+    private MateriaBD materiaBD;
+    private HashMap<String, Integer> spinnerMap = new HashMap<String, Integer>();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carga_academica_insertar);
-        helper = new CargaAcademicaBD(this);
-        editCargo=(EditText)findViewById(R.id.editcargo);
-        editCargo=(EditText)findViewById(R.id.editCargo);
-        editDocente=(EditText)findViewById(R.id.editDocente);
-        editMateria=(EditText)findViewById(R.id.editMateria);
-        editCiclo=(EditText)findViewById(R.id.editCiclo);
+        helper=new CargaAcademicaBD(this);
+        docenteBD =new DocenteBD(this);
+        cargoBD=new CargoDB(this);
+        cicloBD=new CicloBD(this);
+        materiaBD=new MateriaBD(this);
+        List<Docente> docenteList = docenteBD.getDocentes();
+        String[] spinnerResource = new String[docenteList.size()];
+        int i = 0;
+        for (Docente d: docenteList){
+            spinnerMap.put(d.getNomDocente(), i);
+            spinnerResource[i]=d.getNomDocente();
+            i++;
+        }
+        List<Materia>materiaList = materiaBD.getMaterias();
+        String[] spinnerRosurce02=new String[materiaList.size()];
+        int j = 0;
+        for (Materia m: materiaList){
+            spinnerMap.put(m.getNomMateria(), j);
+            spinnerRosurce02[j]=m.getNomMateria();
+            j++;
+        }
+        SpinDocente=(Spinner)findViewById(R.id.spinnerDocente);
+        SpinMateria=(Spinner)findViewById(R.id.spinnerMateria);
+        ArrayAdapter <String> adapter01 =new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerResource);
+        adapter01.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinDocente.setAdapter(adapter01);
+        ArrayAdapter<String> adapter02 =new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerRosurce02);
+        adapter02.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinMateria.setAdapter(adapter02);
+        
     }
-    public void insertarCargaAcademica(View v){
-        String regInsertados;
-        Integer cargo=Integer.valueOf(editCargo.getText().toString());
-        String docente =editDocente.getText().toString();
-        String materia = editMateria.getText().toString();
-        Integer ciclo = Integer.valueOf(editCiclo.getText().toString());
 
-        CargaAcademica cargaAcademica= new CargaAcademica();
-        cargaAcademica.setCargo(cargo);
-        cargaAcademica.setDocente(docente);
-        cargaAcademica.setMateria(materia);
-        cargaAcademica.setCiclo(ciclo);
-        helper.abrir();
-        regInsertados=helper.insertar(cargaAcademica);
-        helper.cerrar();
-        Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
-    }
+
+
     public void limpiarTexto(View v){
-        editCargo.setText("");
-        editDocente.setText("");
-        editMateria.setText("");
-        editCiclo.setText("");
+
     }
 
 }
