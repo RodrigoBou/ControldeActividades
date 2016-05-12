5657ad896d6271b13f02cf434cf1d4286abbd179
yohalmo <yohalmito4@gmail.com>
2016-05-12 16:31:44 -0600

    se agrega multi-idioma(FRANCES, ESPAÑOL, INGLES)

--- app/src/main/java/proyecto/pdm/CRUDTablas/CicloBD.java
+++ app/src/main/java/proyecto/pdm/CRUDTablas/CicloBD.java
@@ -3,6 +3,7 @@ package proyecto.pdm.CRUDTablas;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
@@ -47,34 +48,36 @@ public class CicloBD {
         return regInsertados;
     }
 
-   /*public String actualizar(Ciclo ciclo) {
+    public String actualizar(Ciclo ciclo) {
 
         if (verificarIntegridad(ciclo, 1)) {
             String[] id = {ciclo.getId_ciclo()};
             ContentValues cicl = new ContentValues();
+
             cicl.put("id_ciclo", ciclo.getId_ciclo());
             cicl.put("anio_ciclo", ciclo.getAnio_ciclo());
             cicl.put("ciclo_num", ciclo.getAnio_ciclo());
+
             db.update("Ciclo", cicl, "id_ciclo=?", id);
             return "Registro Actualizado Correctamente";
         } else {
             return "Registro con Codigo ciclo: " + ciclo.getId_ciclo() + "no existe";
         }
 
-    }*/
+    }
 
-    /*public String eliminar(Ciclo ciclo){
+    public String eliminar(Ciclo ciclo) {
 
         String regAfectados = "filas afectadas";
         int contador = 0;
 
         if (verificarIntegridad(ciclo, 1)) {
-            contador+= db.delete("GrupoMateria", "cod_docente='"+ docente.getCodDocente()+"'", null);
+            contador += db.delete("GrupoMateria", "ciclo='" + ciclo.getId_ciclo() + "'", null);
         }
-        contador+=db.delete("Docente", "cod_docente='"+docente.getCodDocente()+"'", null);
-        regAfectados+=contador;
-        return  regAfectados;
-    }*/
+        contador += db.delete("Ciclo", "id_ciclo'" + ciclo.getId_ciclo() + "'", null);
+        regAfectados += contador;
+        return regAfectados;
+    }
 
     public void abrir() {
         controlBD.abrir();
@@ -86,32 +89,32 @@ public class CicloBD {
         return;
     }
 
-    /*public boolean verificarIntegridad(Object dato, int relacion) throws SQLException {
-        switch(relacion){
-            case 1:
-            {
-                //Verificar que exista el Docente
-                Docente docente =(Docente)dato;
-                String[] id = {docente.getCodDocente()};
+    public boolean verificarIntegridad(Object dato, int relacion) throws SQLException {
+        switch (relacion) {
+            case 1: {
+                //Verificar que exista Ciclo
+                Ciclo ciclo = (Ciclo) dato;
+                String[] id = {ciclo.getId_ciclo()};
                 abrir();
-                Cursor c = db.query("Docente",null,"cod_docente=?",id,null,null,null);
-                if(c.moveToFirst()){
-                    //se encontro Docente
+                Cursor c = db.query("Ciclo", null, "id_ciclo=?", id, null, null, null);
+                if (c.moveToFirst()) {
+                    //se encontro Ciclo
                     return true;
                 }
                 return false;
             }
-            default: return false;
+            default:
+                return false;
         }
-    }*/
+    }
 
-    /*public List<Ciclo> getCiclos() {
+    public List<Ciclo> getCiclos() {
         Cursor c = db.query("Ciclo", camposCiclo, null, null, null, null, null, null);
         List<Ciclo> cicloList = new ArrayList<Ciclo>();
         if (c.moveToFirst()) {
             do {
                 Ciclo ciclo = new Ciclo();
-                ciclo.setId_ciclo(c.getInt(0));
+                ciclo.setId_ciclo(c.getString(0));
                 ciclo.setAnio_ciclo(c.getString(1));
                 ciclo.setCiclo_num(c.getString(2));
                 cicloList.add(ciclo);
@@ -119,5 +122,5 @@ public class CicloBD {
 
         }
         return cicloList;
-    }*/
+    }
 }
