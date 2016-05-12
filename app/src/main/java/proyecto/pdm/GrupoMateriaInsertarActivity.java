b62f5daac4908d47cb54864a373d673034e0f815
kaguilar94m <kaguilar94m@gmail.com>
2016-05-12 15:40:36 -0600

    GrupoMateriaInsertarActivity.java

--- app/src/main/java/proyecto/pdm/GrupoMateriaInsertarActivity.java
+++ app/src/main/java/proyecto/pdm/GrupoMateriaInsertarActivity.java
@@ -56,7 +56,10 @@ public class GrupoMateriaInsertarActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grupo_materia_insertar);
         helper = new GrupoMateriaBD(this);
-
+        docenteBD =new DocenteBD(this);
+        cicloBD=new CicloBD(this);
+        materiaBD=new MateriaBD(this);
+        tipoGrupoBD=new TipoGrupoBD(this);
         List<Docente> docenteList = docenteBD.getDocentes();
         String[] spinnerResource = new String[docenteList.size()];
         int i = 0;
@@ -100,9 +103,9 @@ public class GrupoMateriaInsertarActivity extends Activity {
         List<TipoGrupo>tipoGrupoListList = tipoGrupoBD.getTipoGrupos();
         String[] spinnerRosurse04= new String[cicloList.size()];
         int z = 0;
-        for (TipoGrupo c : tipoGrupoListList){
-            spinnerMapTipoGrupo.put(c.getTipoGrupo(), c.getTipoGrupo());
-            spinnerRosurse04[z]=c.getcodTipoGrupo();
+        for (TipoGrupo o : tipoGrupoListList){
+            spinnerMapTipoGrupo.put(o.getTipoGrupo(), o.getTipoGrupo());
+            spinnerRosurse04[z]=o.getcodTipoGrupo();
             z++;
         }
         SpinTipoGrupo=(Spinner)findViewById(R.id.spinnerTipoGrupo);
