72faa298fe7525efd9474043fe4bf9080084b9c0
kaguilar94m <kaguilar94m@gmail.com>
2016-05-12 15:28:15 -0600

    grupo materia insertar.xml

--- app/src/main/java/proyecto/pdm/GrupoMateriaInsertarActivity.java
+++ app/src/main/java/proyecto/pdm/GrupoMateriaInsertarActivity.java
@@ -2,16 +2,40 @@ package proyecto.pdm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.view.View;
+
+import java.util.HashMap;
+import java.util.List;
+
+import proyecto.pdm.CRUDTablas.CicloBD;
+import proyecto.pdm.CRUDTablas.DocenteBD;
 import proyecto.pdm.CRUDTablas.GrupoMateriaBD;
 
+import proyecto.pdm.CRUDTablas.MateriaBD;
+import proyecto.pdm.CRUDTablas.TipoGrupoBD;
+import proyecto.pdm.ClasesModelo.Ciclo;
+import proyecto.pdm.ClasesModelo.Docente;
 import proyecto.pdm.ClasesModelo.GrupoMateria;
+import proyecto.pdm.ClasesModelo.Horario;
+import proyecto.pdm.ClasesModelo.Materia;
+import proyecto.pdm.ClasesModelo.TipoGrupo;
 
 public class GrupoMateriaInsertarActivity extends Activity {
     GrupoMateriaBD helper;
-
+    private Spinner SpinDocente1;
+    private Spinner SpinHorario;
+    private Spinner SpinCiclo1;
+    private Spinner SpinMateria1;
+    private Spinner SpinTipoGrupo;
+    private HashMap<String, String> spinnerMapDocente = new HashMap<String, String>();
+    private HashMap<String, String> spinnerMapMateria = new HashMap<String, String>();
+    private HashMap<String, Integer> spinnerMapHorario = new HashMap<String, Integer>();
+    private HashMap<String, Integer> spinnerMapCiclo= new HashMap<String,Integer>();
+    private HashMap<String,String> spinnerMapTipoGrupo = new HashMap<String, String>();
     EditText editTipoGrupo;
     EditText editIdGrupo;
     EditText editMateria;
@@ -21,46 +45,101 @@ public class GrupoMateriaInsertarActivity extends Activity {
     EditText editDiasImpartida;
     EditText editHorario;
     EditText editNumGrupo;
+    private DocenteBD docenteBD;
+    private CicloBD cicloBD;
+   // private HorarioBD horarioBD;
+    private MateriaBD materiaBD;
+    private TipoGrupoBD tipoGrupoBD;
 
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grupo_materia_insertar);
         helper = new GrupoMateriaBD(this);
-        editTipoGrupo = (EditText) findViewById(R.id.editTipoGrupo);
+
+        List<Docente> docenteList = docenteBD.getDocentes();
+        String[] spinnerResource = new String[docenteList.size()];
+        int i = 0;
+        for (Docente d: docenteList){
+            spinnerMapDocente.put(d.getNomDocente(), d.getCodDocente());
+            spinnerResource[i]=d.getNomDocente();
+            i++;
+        }
+        SpinDocente1=(Spinner)findViewById(R.id.spinnerDocente1);
+        ArrayAdapter<String> adapter01 =new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerResource);
+        adapter01.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinDocente1.setAdapter(adapter01);
+
+        List<Materia>materiaList = materiaBD.getMaterias();
+        String[] spinnerRosurce02=new String[materiaList.size()];
+        int j = 0;
+        for (Materia m: materiaList){
+            spinnerMapMateria.put(m.getNomMateria(), m.getCodMateria());
+            spinnerRosurce02[j]=m.getNomMateria();
+            j++;
+        }
+        SpinMateria1=(Spinner)findViewById(R.id.spinnerMateria1);
+        ArrayAdapter<String> adapter02 =new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerRosurce02);
+        adapter02.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinMateria1.setAdapter(adapter02);
+
+
+        List<Ciclo>cicloList = cicloBD.getCiclos();
+        Integer[] spinnerRosurse03= new Integer[cicloList.size()];
+        int k = 0;
+        for (Ciclo c : cicloList){
+            spinnerMapCiclo.put(c.getCiclo_num(),c.getId_ciclo());
+            spinnerRosurse03[k]=c.getId_ciclo();
+            k++;
+        }
+        SpinCiclo1=(Spinner)findViewById(R.id.spinnerCiclo1);
+        ArrayAdapter<Integer> adapter03 =new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item,  spinnerRosurse03);
+        adapter03.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinCiclo1.setAdapter(adapter03);
+
+        List<TipoGrupo>tipoGrupoListList = tipoGrupoBD.getTipoGrupos();
+        String[] spinnerRosurse04= new String[cicloList.size()];
+        int z = 0;
+        for (TipoGrupo c : tipoGrupoListList){
+            spinnerMapTipoGrupo.put(c.getTipoGrupo(), c.getTipoGrupo());
+            spinnerRosurse04[z]=c.getcodTipoGrupo();
+            z++;
+        }
+        SpinTipoGrupo=(Spinner)findViewById(R.id.spinnerTipoGrupo);
+        ArrayAdapter<String> adapter04 =new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,  spinnerRosurse04);
+        adapter04.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinTipoGrupo.setAdapter(adapter04);
+
+
         editIdGrupo = (EditText) findViewById(R.id.editIdGrupo);
-        editMateria = (EditText) findViewById(R.id.editMateria);
-        editDocente = (EditText) findViewById(R.id.editDocente);
-        editCiclo = (EditText) findViewById(R.id.editCiclo);
-        editLocal = (EditText) findViewById(R.id.editIdGrupo);
-        editDiasImpartida = (EditText) findViewById(R.id.editMateria);
-        editHorario= (EditText) findViewById(R.id.editDocente);
-        editNumGrupo= (EditText) findViewById(R.id.editCiclo);
+        editLocal = (EditText) findViewById(R.id.editLocal);
+        editDiasImpartida = (EditText) findViewById(R.id.editDiasImpartida);
+        editNumGrupo= (EditText) findViewById(R.id.editNumGrupo);
 
 
     }
 
     public void insertarGrupoMateria(View v){
         String idGrupo = editIdGrupo.getText().toString();
-        String tipoGrupo = editTipoGrupo.getText().toString();
-        String materia = editMateria.getText().toString();
-        String docente = editDocente.getText().toString();
-        String ciclo = editCiclo.getText().toString();
+        String tipoGrupo = SpinTipoGrupo.getSelectedItem().toString();
+        String materia = SpinMateria1.getSelectedItem().toString();
+        String docente = SpinDocente1.getSelectedItem().toString();
+        String ciclo = SpinCiclo1.getSelectedItem().toString();
         String local = editLocal.getText().toString();
         String diasImpartida = editDiasImpartida.getText().toString();
-        String horario = editHorario.getText().toString();
+        String horario = SpinHorario.getSelectedItem().toString();
         String numGrupo = editNumGrupo.getText().toString();
         String regInsertados;
 
         GrupoMateria grupoMateria = new GrupoMateria();
         grupoMateria.setIdGrupo(Integer.parseInt(idGrupo));
-        grupoMateria.setTipoGrupo(tipoGrupo);
-        grupoMateria.setMateria(materia);
-        grupoMateria.setDocente(docente);
-        grupoMateria.setCiclo(Integer.parseInt(ciclo));
+        grupoMateria.setTipoGrupo(spinnerMapTipoGrupo.get(tipoGrupo));
+        grupoMateria.setMateria(spinnerMapMateria.get(materia));
+        grupoMateria.setDocente(spinnerMapDocente.get(docente));
+        grupoMateria.setCiclo(spinnerMapCiclo.get(ciclo));
         grupoMateria.setLocal(local);
         grupoMateria.setDiasImpartida(diasImpartida);
-        grupoMateria.setHorario(Integer.parseInt(horario));
+        grupoMateria.setHorario(spinnerMapHorario.get(horario));
         grupoMateria.setNumGrupo(numGrupo);
 
         regInsertados=helper.insertar(grupoMateria);
