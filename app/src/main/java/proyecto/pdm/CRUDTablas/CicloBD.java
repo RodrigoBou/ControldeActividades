8b9adde98398d73f150f53b84ed281a182c67c80
Rodrigo <racb1994@gmail.com>
2016-05-12 02:05:32 -0600

    se agrega multi-idioma(FRANCES, ESPAÑOL, INGLES)

--- app/src/main/java/proyecto/pdm/CRUDTablas/CicloBD.java
+++ app/src/main/java/proyecto/pdm/CRUDTablas/CicloBD.java
@@ -1,5 +1,6 @@
 package proyecto.pdm.CRUDTablas;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -19,25 +20,104 @@ public class CicloBD {
     private SQLiteDatabase db;
     private ControlBD controlBD;
 
-    public CicloBD(Context ctx){
+    public CicloBD(Context ctx) {
         controlBD = new ControlBD(ctx);
         db = controlBD.getDb();
 
     }
 
-    public List<Ciclo> getCiclos() {
-        Cursor c= db.query("Ciclo", camposCiclo,null,null,null,null,null,null);
+    public String insertar(Ciclo ciclo) {
+        String regInsertados = "Registro Insertado No= ";
+        long contador = 0;
+
+        ContentValues cicl = new ContentValues();
+
+        cicl.put("id_ciclo", ciclo.getId_ciclo());
+        cicl.put("anio_ciclo", ciclo.getAnio_ciclo());
+        cicl.put("ciclo_num", ciclo.getCiclo_num());
+
+        contador = db.insert("Ciclo", null, cicl);
+
+        if (contador == -1 || contador == 0) {
+            regInsertados = "Error al Insertar el registro, Registro Duplicado.Verificar inserción ";
+        } else {
+            regInsertados = regInsertados + contador;
+        }
+
+        return regInsertados;
+    }
+
+   /*public String actualizar(Ciclo ciclo) {
+
+        if (verificarIntegridad(ciclo, 1)) {
+            String[] id = {ciclo.getId_ciclo()};
+            ContentValues cicl = new ContentValues();
+            cicl.put("id_ciclo", ciclo.getId_ciclo());
+            cicl.put("anio_ciclo", ciclo.getAnio_ciclo());
+            cicl.put("ciclo_num", ciclo.getAnio_ciclo());
+            db.update("Ciclo", cicl, "id_ciclo=?", id);
+            return "Registro Actualizado Correctamente";
+        } else {
+            return "Registro con Codigo ciclo: " + ciclo.getId_ciclo() + "no existe";
+        }
+
+    }*/
+
+    /*public String eliminar(Ciclo ciclo){
+
+        String regAfectados = "filas afectadas";
+        int contador = 0;
+
+        if (verificarIntegridad(ciclo, 1)) {
+            contador+= db.delete("GrupoMateria", "cod_docente='"+ docente.getCodDocente()+"'", null);
+        }
+        contador+=db.delete("Docente", "cod_docente='"+docente.getCodDocente()+"'", null);
+        regAfectados+=contador;
+        return  regAfectados;
+    }*/
+
+    public void abrir() {
+        controlBD.abrir();
+        return;
+    }
+
+    public void cerrar() {
+        controlBD.cerrar();
+        return;
+    }
+
+    /*public boolean verificarIntegridad(Object dato, int relacion) throws SQLException {
+        switch(relacion){
+            case 1:
+            {
+                //Verificar que exista el Docente
+                Docente docente =(Docente)dato;
+                String[] id = {docente.getCodDocente()};
+                abrir();
+                Cursor c = db.query("Docente",null,"cod_docente=?",id,null,null,null);
+                if(c.moveToFirst()){
+                    //se encontro Docente
+                    return true;
+                }
+                return false;
+            }
+            default: return false;
+        }
+    }*/
+
+    /*public List<Ciclo> getCiclos() {
+        Cursor c = db.query("Ciclo", camposCiclo, null, null, null, null, null, null);
         List<Ciclo> cicloList = new ArrayList<Ciclo>();
-        if (c.moveToFirst()){
+        if (c.moveToFirst()) {
             do {
-                Ciclo ciclo= new Ciclo();
+                Ciclo ciclo = new Ciclo();
                 ciclo.setId_ciclo(c.getInt(0));
-               ciclo.setAnio_ciclo(c.getString(1));
+                ciclo.setAnio_ciclo(c.getString(1));
                 ciclo.setCiclo_num(c.getString(2));
                 cicloList.add(ciclo);
-            }while (c.moveToNext());
+            } while (c.moveToNext());
 
         }
         return cicloList;
-    }
+    }*/
 }
--- app/src/main/java/proyecto/pdm/ClasesModelo/Ciclo.java
+++ app/src/main/java/proyecto/pdm/ClasesModelo/Ciclo.java
@@ -2,27 +2,29 @@ package proyecto.pdm.ClasesModelo;
 
 /**
  * Created by kelly on 23/04/2016.
+ * and
+ * Modified by Yohalmo
  */
 public class Ciclo {
 
-    private int id_ciclo;
+    private String id_ciclo;
     private String anio_ciclo;
     private String ciclo_num;
 
     public Ciclo() {
     }
 
-    public Ciclo(int id_ciclo, String anio_ciclo, String ciclo_num) {
+    public Ciclo(String id_ciclo, String anio_ciclo, String ciclo_num) {
         this.id_ciclo = id_ciclo;
         this.anio_ciclo = anio_ciclo;
         this.ciclo_num = ciclo_num;
     }
 
-    public int getId_ciclo() {
+    public String getId_ciclo() {
         return id_ciclo;
     }
 
-    public void setId_ciclo(int id_ciclo) {
+    public void setId_ciclo(String id_ciclo) {
         this.id_ciclo = id_ciclo;
     }
 
